0e4a3c376e8b9513c186fc5d91a50b5417b501a8
markoclimako <mkclimako@gmail.com>
2023-09-15 20:34:08 -0300

    feat: add functionality to load more news when clicking on the Mais Noticias button

--- src/pages/Releases/index.tsx
+++ src/pages/Releases/index.tsx
@@ -1,28 +1,47 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { releaseUrl } from '../../api'
+import Button from '../../components/Button'
 import { NewsCard } from '../../components/NewsCard'
 import { NewsContext } from '../../context/NewsContext'
 import { NewsItem } from '../../hooks/types'
 import { useNewsFetch } from '../../hooks/useNewsFetch'
-import { Container } from './latest.styles'
+import { Container, NewsContainer } from './latest.styles'
+
+const initialNewsCount = 20
 
 export const Releases = () => {
   const news = useNewsFetch(releaseUrl)
   const { favorites, handleFavoriteNews } = useContext(NewsContext)
+  const [displayedNews, setDisplayedNews] = useState(initialNewsCount)
 
+  const handleLoadMoreNews = () => {
+    const newsPerLoad = 20
+    setDisplayedNews(displayedNews + newsPerLoad)
+  }
   return (
     <Container>
-      {news &&
-        news.map((newsItem: NewsItem) => (
-          <NewsCard
-            key={newsItem.id}
-            newsItem={newsItem}
-            handleFavorite={handleFavoriteNews}
-            isFavorite={favorites.some(
-              (favorite) => favorite.id === newsItem.id,
-            )}
-          />
-        ))}
+      <NewsContainer>
+        {news &&
+          news
+            .slice(0, displayedNews)
+            .map((newsItem: NewsItem) => (
+              <NewsCard
+                key={newsItem.id}
+                newsItem={newsItem}
+                handleFavorite={handleFavoriteNews}
+                isFavorite={favorites.some(
+                  (favorite) => favorite.id === newsItem.id,
+                )}
+              />
+            ))}
+      </NewsContainer>
+      {displayedNews < news.length && (
+        <Button
+          role="read more"
+          title="MAIS NOTÍCIAS"
+          onClick={handleLoadMoreNews}
+        />
+      )}
     </Container>
   )
 }
