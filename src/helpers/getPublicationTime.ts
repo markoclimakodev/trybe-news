fa25d691e1b8ade127b50f2fdbd08ef670b60da5
markoclimako <mkclimako@gmail.com>
2023-09-15 17:41:49 -0300

    refactor: getPublicationTime function

--- src/helpers/getPublicationTime.ts
+++ src/helpers/getPublicationTime.ts
@@ -1,9 +1,4 @@
-import {
-  differenceInDays,
-  differenceInMonths,
-  differenceInYears,
-  parse,
-} from 'date-fns'
+import { differenceInDays, parse } from 'date-fns'
 
 export const getPublicationTime = (dataPublication: string): string => {
   const publicationDate = parse(
@@ -14,25 +9,16 @@ export const getPublicationTime = (dataPublication: string): string => {
   const currentDate = new Date()
 
   const day = differenceInDays(currentDate, publicationDate)
-  const month = differenceInMonths(currentDate, publicationDate)
-  const year = differenceInYears(currentDate, publicationDate)
 
   let message = ''
 
-  if (year > 0) {
-    message = `${year} ${year === 1 ? 'ano' : 'anos'} atrás`
-  }
-
-  if (month > 0) {
-    message = `${month} ${month === 1 ? 'mês' : 'meses'} atrás`
+  if (day === 0) {
+    message = `${day + 1} dia atrás`
   }
 
   if (day > 0) {
     message = `${day} ${day === 1 ? 'dia' : 'dias'} atrás`
   }
 
-  if (day === 0) {
-    message = `${day + 1} dia atrás`
-  }
   return message
 }
