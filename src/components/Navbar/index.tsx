b36f87e7c0de0351efea53f63aa8fdec67f40a10
markoclimako <mkclimako@gmail.com>
2023-09-15 18:42:36 -0300

    refactor: remove fetch logic from Navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,46 +1,20 @@
-import { useContext, useEffect, useState } from 'react'
-import { apiEndPoint } from '../../api'
-import { NewsContext } from '../../context/NewsContext'
-import { useNewsFetch } from '../../hooks/useNewsFetch'
 import { Category, CategoryContainer, Container } from './navbar.styles'
-import { ClickType } from './types'
 
 export const Navbar = () => {
-  const { handleNews } = useContext(NewsContext)
-  const [newsType, setNewsType] = useState('latest')
-  const news = useNewsFetch(apiEndPoint[newsType])
-
-  const handleNewsType = ({ currentTarget }: ClickType) => {
-    const { id } = currentTarget
-    setNewsType(id)
-  }
-
-  useEffect(() => {
-    handleNews(news)
-  }, [handleNews, news])
-
   return (
     <Container>
       <CategoryContainer>
         <Category>
-          <button onClick={handleNewsType} type="button" id="latest">
-            Mais Recentes
-          </button>
+          <a href="/">Mais Recentes</a>
         </Category>
         <Category>
-          <button onClick={handleNewsType} type="button" id="release">
-            Releases
-          </button>
+          <a href="/releases">Releases</a>
         </Category>
         <Category>
-          <button onClick={handleNewsType} type="button" id="news">
-            Notícias
-          </button>
+          <a href="/news">Notícias</a>
         </Category>
         <Category>
-          <button onClick={handleNewsType} type="button" id="favorites">
-            Favoritas
-          </button>
+          <a href="/favorites">Favoritas</a>
         </Category>
       </CategoryContainer>
     </Container>
