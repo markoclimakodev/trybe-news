073a0025aa399ffd36fde896428a8bd748ed27bf
markoclimako <mkclimako@gmail.com>
2023-09-15 17:53:40 -0300

    refactor: update NewsItem type

--- src/hooks/types.ts
+++ src/hooks/types.ts
@@ -1,23 +1,16 @@
-export interface NewsItem {
+export type NewsItem = {
   id: number
   tipo: string
   titulo: string
   introducao: string
   data_publicacao: string
   produto_id: number
-  produtos: string
-  editorias: string
   imagens: {
     image_intro: string
-    float_intro: string
     image_intro_alt: string
     image_intro_caption: string
-    image_fulltext: string
     float_fulltext: string
-    image_fulltext_alt: string
-    image_fulltext_caption: string
   }
-  produtos_relacionados: string
   destaque: boolean
   link: string
 }
