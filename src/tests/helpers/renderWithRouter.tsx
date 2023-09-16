7bb41f99871cefe91e99127481bab24189093b2f
markoclimako <mkclimako@gmail.com>
2023-09-16 20:03:35 -0300

    feat: add NewsProvider  to renderWithRouter helper function

--- src/tests/helpers/renderWithRouter.tsx
+++ src/tests/helpers/renderWithRouter.tsx
@@ -1,13 +1,18 @@
 import { render } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { BrowserRouter } from 'react-router-dom'
+import { NewsProvider } from '../../context/NewsProvider'
 
 const renderWithRouter = (ui: JSX.Element, { route = '/' } = {}) => {
   window.history.pushState({}, '', route)
 
   return {
     user: userEvent.setup(),
-    ...render(ui, { wrapper: BrowserRouter }),
+    ...render(
+      <BrowserRouter>
+        <NewsProvider>{ui}</NewsProvider>
+      </BrowserRouter>,
+    ),
   }
 }
 
