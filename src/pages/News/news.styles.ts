35b4b9f322bcd9b73fabde602473dce4193029ea
markoclimako <mkclimako@gmail.com>
2023-09-15 21:00:44 -0300

    style: change padding

--- src/pages/News/news.styles.ts
+++ src/pages/News/news.styles.ts
@@ -4,7 +4,7 @@ export const Container = styled.main`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 3.2rem;
+  padding: 0 0 3.2rem;
 `
 
 export const NewsContainer = styled.section`
