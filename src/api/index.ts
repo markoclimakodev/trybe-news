63f6f826a6975183e60bd930b2b4e9f314ceb44e
markoclimako <mkclimako@gmail.com>
2023-10-10 20:38:07 -0300

    change api return qtd

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,5 @@
 export const newsUrl =
-  'https://servicodados.ibge.gov.br/api/v3/noticias/?tipo=noticia&qtd=100'
+  'https://servicodados.ibge.gov.br/api/v3/noticias/?tipo=noticia&qtd=80'
 
 export const releaseUrl =
-  'https://servicodados.ibge.gov.br/api/v3/noticias/?tipo=release&qtd=100'
+  'https://servicodados.ibge.gov.br/api/v3/noticias/?tipo=release&qtd=80'
